3bfddfd82a7a576fc282f5ff1da22102e792b0dd
humaf <qfhuma@gmail.com>
2018-03-21 07:20:48 -0700

    Added a java Library

---
+++ javaJokes/src/main/java/builditbigger/strawbericreations/com/javajokes/Joker.java
@@ -0,0 +1,10 @@
+package builditbigger.strawbericreations.com.javajokes;
+
+public class Joker {
+
+    public String getJoke() {
+        return "I am functional Adult but my phone changed me to a fictional adult,and i feel thats mpre accurate";
+    }
+
+    }
+
